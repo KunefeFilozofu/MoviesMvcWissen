dd7a969e2fbbc503ff8dd0fa11c2ee5e6aedc0fe
Sami Çiçekli <admin@samicicekli.com.tr>
2020-02-05 21:26:16 +0300

    Add project files.

---
+++ 036_MoviesMvcWissen/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _036_MoviesMvcWissen.Contexts;
+using _036_MoviesMvcWissen.Entities;
+
+namespace _036_MoviesMvcWissen.Controllers
+{
+    public class MoviesController : Controller
+    {
+        MoviesContext db = new MoviesContext();
+
+        public ViewResult Index()
+        {
+            var model = db.Movies.ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(string inputName, string inputProductionYear, string inputBoxOfficeReturn)
+        {
+            var entity = new Movie()
+            {
+                Name = inputName,
+                ProductionYear = inputProductionYear,
+                BoxOfficeReturn = Convert.ToDouble(inputBoxOfficeReturn.Replace(",", "."), CultureInfo.InvariantCulture)
+            };
+
+            db.Movies.Add(entity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
